1e4808d957e839805d903a08d8f18ef59690a385
KekeH <kea@live.fi>
2015-08-18 09:53:57 +0300

    Updated 0.0.3.

--- demo/vstags-0.0.3.js
+++ demo/vstags-0.0.3.js
@@ -1,13 +1,13 @@
 /* 
 *  Name: vstags 
 *  Description: Tags - AngularJS reusable UI component 
-*  Version: 0.0.2 
+*  Version: 0.0.3 
 *  Author: kekeh 
 *  Homepage: http://kekeh.github.io/vstags 
 *  License: MIT 
-*  Date: 2015-08-17 
+*  Date: 2015-08-18 
 */ 
-angular.module('template-vstags-0.0.2.html', ['templates/vstags.html']);
+angular.module('template-vstags-0.0.3.html', ['templates/vstags.html']);
 
 angular.module("templates/vstags.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("templates/vstags.html",
@@ -18,14 +18,14 @@ angular.module("templates/vstags.html", []).run(["$templateCache", function($tem
     "            <div ng-include=\"'tagsrepeat.html'\"></div>\n" +
     "        </div>\n" +
     "        <span class=\"vsbtncontainer\">\n" +
-    "            <button class=\"vstagsbtn\" ng-click=\"showOverlay=true\" ng-show=\"selectedTags.length>0\"><span class=\"icon icon-selections\"></span></button>\n" +
+    "            <button class=\"vstagsbtn\" ng-click=\"showOverlay=true\" ng-show=\"showTagsBtn\"><span class=\"icon icon-selections\"></span></button>\n" +
     "            <button class=\"vstagsbtn\" ng-click=\"addNewItem()\"><span class=\"icon\" ng-class=\"{'icon-plus':!showInput,'icon-check':showInput}\"></span></button>\n" +
-    "            <span class=\"vstagsselectioncounttxt\" ng-if=\"selectedTags.length > 0\" ng-click=\"showOverlay=true\">{{selectedTags.length}}</span>\n" +
+    "            <span class=\"vstagsselectioncounttxt\" ng-if=\"showTagsBtn\" ng-click=\"showOverlay=true\">{{selectedTags.length}}</span>\n" +
     "        </span>\n" +
     "    </div>\n" +
     "    <input class=\"vstagsinput\" type=\"text\" placeholder=\"{{conf.TYPE_TAG_TXT}}\" input-focus ng-show=\"showInput\" ng-model=\"inputTxt\" ng-model-options=\"{debounce:conf.INPUT_DEBOUNCE}\" ng-keydown=\"keyDown($event)\"/>\n" +
     "    <div class=\"vstagsloader\" ng-show=\"showInput&&inputTxt!==''&&loadedTags.length>0\">\n" +
-    "        <div class=\"vstagsloaderitem\" ng-repeat=\"item in loadedTags track by $index\" ng-click=\"itemClicked(item)\">\n" +
+    "        <div class=\"vstagsloaderitem\" ng-repeat=\"item in loadedTags track by $index\" ng-click=\"itemClicked(item)\" ng-keydown=\"$event.which===13?itemClicked(item):null\" tabindex=\"0\">\n" +
     "            <span class=\"vstagsloaderitemtxt\">{{item}}</span>\n" +
     "        </div>\n" +
     "    </div>\n" +
@@ -33,7 +33,7 @@ angular.module("templates/vstags.html", []).run(["$templateCache", function($tem
     "        <div class=\"vstagsoverlay\" ng-mouseleave=\"closeOverlay()\">\n" +
     "            <div class=\"vstagsoverlaytitle\">\n" +
     "                <span class=\"vstagsoverlaytitletxt\">{{selectedTags.length}} {{conf.SEL_TAGS_TXT}}</span>\n" +
-    "                <span class=\"icon icon-cross vsiconoverlaycross\" ng-click=\"closeOverlay()\"></span>\n" +
+    "                <span class=\"icon icon-cross vsiconoverlaycross\" ng-click=\"closeOverlay()\" ng-keydown=\"$event.which===13?closeOverlay():null\" tabindex=\"0\"></span>\n" +
     "            </div>\n" +
     "            <div ng-include=\"'tagsrepeat.html'\"></div>\n" +
     "        </div>\n" +
@@ -41,20 +41,20 @@ angular.module("templates/vstags.html", []).run(["$templateCache", function($tem
     "    <script type=\"text/ng-template\" id=\"tagsrepeat.html\">\n" +
     "        <div class=\"vsselectedtags\" ng-repeat=\"item in selectedTags track by $index\">\n" +
     "            <div class=\"vstagtext\">{{item}}</div>\n" +
-    "            <span class=\"vsiconcross icon icon-cross\" ng-click=\"removeItem(item)\"></span>\n" +
+    "            <span class=\"vsiconcross icon icon-cross\" ng-click=\"removeItem(item)\" ng-keydown=\"$event.which===13?removeItem(item):null\" tabindex=\"0\"></span>\n" +
     "        </div>\n" +
     "    </script>\n" +
     "</div>");
 }]);
 
-angular.module('vstags', ["template-vstags-0.0.2.html"])
+angular.module('vstags', ["template-vstags-0.0.3.html"])
     .constant('vstagsConf', {
         INPUT_DEBOUNCE: 500,
         INPUT_FOCUS_EVENT: 'vstags.ife',
-        SEL_TAGS_TXT: 'selected tag(s)',
+        SEL_TAGS_TXT: 'tag(s)',
         TYPE_TAG_TXT: 'Type tag here...'
     })
-    .directive('vstags', function () {
+    .directive('vstags', ['$window', function ($window) {
         return {
             restrict: 'EA',
             templateUrl: 'templates/vstags.html',
@@ -72,7 +72,8 @@ angular.module('vstags', ["template-vstags-0.0.2.html"])
             link: function (scope, element, attrs) {
                 scope.selectedTags = [], scope.loadedTags = [];
                 scope.showInput = false, scope.showOverlay = false;
-                var inputTxtWatch = null;
+                var tagsArea = tagsArea = angular.element(element[0].querySelector('.vstagsarea'));
+                var window = angular.element($window);
 
                 scope.addNewItem = function () {
                     scope.showInput = !scope.showInput;
@@ -111,10 +112,15 @@ angular.module('vstags', ["template-vstags-0.0.2.html"])
                     scope.showOverlay = false;
                 };
 
-                scope.$on('$destroy', function () {
-                    if (inputTxtWatch !== null) {
-                        inputTxtWatch();
-                    }
+                scope.$watch(function () {
+                    return tagsArea[0].scrollHeight;
+                },
+                function (val) {
+                    scope.showTagsBtn = val > 34;
+                });
+
+                window.on('resize', function () {
+                    scope.$apply();
                 });
 
                 function inputTxtWatchFn(newVal, oldVal) {
@@ -133,8 +139,8 @@ angular.module('vstags', ["template-vstags-0.0.2.html"])
                 }
 
                 function init() {
-                    if(attrs.onLoadTagsFn !== undefined) {
-                        inputTxtWatch = scope.$watch('inputTxt', inputTxtWatchFn);
+                    if (attrs.onLoadTagsFn !== undefined) {
+                        scope.$watch('inputTxt', inputTxtWatchFn);
                     }
                     scope.selectedTags = scope.ngModel;
                 }
@@ -142,7 +148,7 @@ angular.module('vstags', ["template-vstags-0.0.2.html"])
                 init();
             }
         };
-    })
+    }])
 
 /**
  * @ngdoc object
